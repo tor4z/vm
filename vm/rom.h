0eab93883dc255ddd1a28896a02729386be0e21b
tor4z <vwenjie@hotmail.com>
2018-06-02 06:05:54 +0800

    Fix oprerator enum

--- vm/rom.h
+++ vm/rom.h
@@ -2,18 +2,18 @@
 #define _ROM_H
 
 enum oprerator {
-    ADD   = 0x01;
-    SUB   = 0x02;
-    MOV   = 0x03;
-    JZ    = 0x04;
-    POP   = 0x05;
-    PUSH  = 0x06;
-    CALL  = 0x07;
-    JUMP  = 0x08;
-    DIS   = 0x09;
-    LOAD  = 0x0a;
-    STORE = 0x0b;
-    RET   = 0x0c;
+    ADD   = 0x01,
+    SUB   = 0x02,
+    MOV   = 0x03,
+    JZ    = 0x04,
+    POP   = 0x05,
+    PUSH  = 0x06,
+    CALL  = 0x07,
+    JUMP  = 0x08,
+    DIS   = 0x09,
+    LOAD  = 0x0a,
+    STORE = 0x0b,
+    RET   = 0x0c
 };
 
 #endif
