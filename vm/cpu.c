2a3d1314f6823c02e8ec92ab028c6a8dd1ac773e
tor4z <vwenjie@hotmail.com>
2018-06-02 18:44:30 +0800

    Update init cpu

--- vm/cpu.c
+++ vm/cpu.c
@@ -3,6 +3,9 @@
 #include "vm.h"
 
 
+static void init_bp(struct cpu *cpu);
+
+
 struct cpu * new_cpu()
 {
 	struct cpu *cpu = (struct cpu*)malloc(sizeof(struct cpu));
@@ -20,4 +23,16 @@ void run_ins(struct cpu *cpu, struct ins *ins)
 void free_cpu(struct cpu *cpu)
 {
 	free(cpu);
+}
+
+
+static void init_bp(struct cpu *cpu)
+{
+    cpu->registers[BP_ADDR] = MAX_STACK;
+}
+
+
+void init_cpu(struct cpu *cpu)
+{
+	init_bp(cpu);
 }
