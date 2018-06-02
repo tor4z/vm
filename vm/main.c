03e1905f5ee8f76c87ea350b08a0d2504620656c
tor4z <vwenjie@hotmail.com>
2018-06-02 18:50:40 +0800

    Update main

--- vm/main.c
+++ vm/main.c
@@ -3,5 +3,15 @@
 
 int main()
 {
+	struct cpu *cpu;
+	struct rom *rom;
+	struct ram *ram;
+	struct bus *bus;
+
+	cpu = new_cpu();
+	ram = new_ram();
+	rom = new_rom();
+	bus = new_bus();
+
 	return 0;
 }
